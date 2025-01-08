e269f0148a8d7d95a7295ba54d60874e5638a3ad
alifatehb <alifateh35790@gmail.com>
2025-01-08 12:13:17 +0330

    abroun forced edit

--- app/database/models.py
+++ app/database/models.py
@@ -9,3 +9,12 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     name = Column(String)
     password = Column(String)
+#.................................................
+class Car(Base):
+    __tablename__ = "cars"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    color = Column(String)
+    plate = Column(String)
+    ownerId = Column(Integer)
--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -19,3 +19,25 @@ class User(UserBase):
 
     class Config:
         orm_mode = True
+#.................................
+class CarBase(BaseModel):
+    name: str
+    color: str
+    plate: str
+    ownerId: int
+
+class CarCreate(CarBase):
+    pass
+
+class CarUpdate(BaseModel):
+    name: Optional[str] = None
+    color: Optional[str] = None
+    plate: Optional[str] = None
+    ownerId: Optional[int] = None
+
+class Car(CarBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
