aad3af65b13706868edd0545706fd65907fb9495
alifatehb <alifateh35790@gmail.com>
2025-01-08 12:12:18 +0330

    abroun forced edit

--- app/main.py
+++ app/main.py
@@ -42,3 +42,32 @@ def delete_user(user_id: int, db: Session = Depends(get_db)):
     if not success:
         raise HTTPException(status_code=404, detail="User not found")
     return {"message": "User deleted successfully"}
+#..........................................................
+@app.post("/cars/", response_model=schemas.Car)
+def create_car(car: schemas.CarCreate, db: Session = Depends(get_db)):
+    return crud.create_car(db=db, car=car)
+
+@app.get("/cars/", response_model=list[schemas.Car])
+def read_cars(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_cars(db, skip=skip, limit=limit)
+
+@app.get("/cars/{car_id}", response_model=schemas.Car)
+def read_car(car_id: int, db: Session = Depends(get_db)):
+    db_car = crud.get_car(db, car_id=car_id)
+    if db_car is None:
+        raise HTTPException(status_code=404, detail="Car not found")
+    return db_car
+
+@app.put("/cars/{car_id}", response_model=schemas.Car)
+def update_car(car_id: int, car: schemas.CarUpdate, db: Session = Depends(get_db)):
+    db_car = crud.update_car(db, car_id=car_id, car=car)
+    if db_car is None:
+        raise HTTPException(status_code=404, detail="Car not found")
+    return db_car
+
+@app.delete("/cars/{car_id}")
+def delete_car(car_id: int, db: Session = Depends(get_db)):
+    success = crud.delete_car(db, car_id=car_id)
+    if not success:
+        raise HTTPException(status_code=404, detail="Car not found")
+    return {"message": "Car deleted successfully"}
